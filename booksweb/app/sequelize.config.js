ff87684d203f8f52c75d91f8da9d795ade035cc3
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-10-15 14:26:43 +0530

    day#4 update#4

--- booksweb/app/sequelize.config.js
+++ booksweb/app/sequelize.config.js
@@ -1,9 +1,9 @@
 
 module.exports={
     
-    "db": "booksdb_lnt",
-    "user":"root",
-    "password": "@DM1n.",
+    "db": process.env.DB_SERVER,
+    "user":process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
     "options":{
         "host":"localhost",
         "dialect":"mysql"
--- booksweb/app/server.js
+++ booksweb/app/server.js
@@ -2,6 +2,9 @@ let express=require('express');
 let app=express()
 let bodyParser=require('body-parser');
 
+//read .env file and get the necessary details
+require('dotenv').config(); 
+
 //no need to cache return of this require
 //this is to trigger sequelize initilization for our app
 require('./models/sequelize-bootsrap'); //will initialize sequelize 
